907985370324073109c42abbf685ee60a7bc9cba
nsengu2619 <neenas@umd.edu>
2024-03-28 19:29:54 -0400

    Added code to understand the flask application and creating a base template for my child pages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,5 +17,25 @@ def about():  # put application's code here
 def about_css():  # put application's code here
     return render_template('about-css.html')
 
+@app.route('/favorite-course')
+def favorite_course():  # put application's code here
+    course = request.args.get('course')
+    course_number = request.args.get('course_number')
+
+    return render_template('favorite-course.html', course=course, course_number=course_number)
+
+@app.route('/test')
+def test():  # put application's code here
+    return render_template('test.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+@app.route('/base')
+def base():
+    return render_template('base.html')
 if __name__ == '__main__':
     app.run()
